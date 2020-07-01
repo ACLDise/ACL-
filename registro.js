60aff60360036ee51303e5858d34392952f7e240
ACLDise <67666036+ACLDise@users.noreply.github.com>
2020-06-30 19:57:47 -0500

    Add files via upload

---
+++ registro.js
@@ -0,0 +1,46 @@
+var datos = function(){
+	var nombre = document.getElementById("itxt_nombre").value;
+	console.log("Nombre:" +nombre+"\n");
+
+	var apellidos = document.getElementById("itxt_apellido").value;
+	console.log("Apellidos:" +apellidos+"\n");
+
+
+	var edad = document.getElementById("edad_mayor").value;
+	console.log("Edad:" +edad+"\n");
+
+
+	var edad = document.getElementById("edad_menor").value;
+	console.log("Edad:" +edad+"\n");
+
+
+	var documento = document.getElementById("itxt_numeroD").value;
+	console.log("Documento:" +documento+"\n");
+
+
+	var telefono = document.getElementById("itxt_telefono").value;
+	console.log("Telefono:" +telefono+"\n");
+
+
+	var ciudad = document.getElementById("itxt_ciudad").value;
+	console.log("Ciudad:" +ciudad+"\n");
+
+
+	var direccion = document.getElementById("itxt_direccion").value;
+	console.log("Direccion:" +direccion+"\n");
+
+
+	var email = document.getElementById("itxt_email").value;
+	console.log("Email:" +email+"\n");
+
+
+	var usuario = document.getElementById("itxt_usuario").value;
+	console.log("Usuario:" +usuario+"\n");
+
+
+	var contraseña = document.getElementById("itxt_contraseña").value;
+	console.log("Contraseña:" +contraseña+"\n");
+
+
+	
+}
